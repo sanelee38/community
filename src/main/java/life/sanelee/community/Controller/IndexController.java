9eb0afaaf66e05d9a959eaede9e2c325b3fb895c
sanelee38 <1870731037@163.com>
2019-07-19 18:34:44 +0800

    add page&dropdown

--- src/main/java/life/sanelee/community/Controller/IndexController.java
+++ src/main/java/life/sanelee/community/Controller/IndexController.java
@@ -1,5 +1,6 @@
 package life.sanelee.community.Controller;
 
+import life.sanelee.community.dto.PaginationDTO;
 import life.sanelee.community.dto.QuestionDTO;
 import life.sanelee.community.mapper.QuestionMapper;
 import life.sanelee.community.mapper.UserMapper;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -27,7 +29,10 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size
+    ) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -42,8 +47,8 @@ public class IndexController {
             }
 
 
-        List<QuestionDTO> questionsList = questionService.list();
-            model.addAttribute("questions",questionsList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
--- src/main/java/life/sanelee/community/service/QuestionService.java
+++ src/main/java/life/sanelee/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package life.sanelee.community.service;
 
+import life.sanelee.community.dto.PaginationDTO;
 import life.sanelee.community.dto.QuestionDTO;
 import life.sanelee.community.mapper.QuestionMapper;
 import life.sanelee.community.mapper.UserMapper;
@@ -21,18 +22,31 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount, page, size);
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+        //size*(page-1)
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
             BeanUtils.copyProperties(question, questionDTO);
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
-
         }
 
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
     }
 }
