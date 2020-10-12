1da6ddfbb48f0c57da7fde5739ae9f04e092be73
sanelee38 <18710731037@163.com>
2020-10-12 18:08:02 +0800

    change page size

--- src/main/java/life/sanelee/community/Controller/IndexController.java
+++ src/main/java/life/sanelee/community/Controller/IndexController.java
@@ -23,7 +23,7 @@ public class IndexController {
     public String index(HttpServletRequest request,
                         Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
-                        @RequestParam(name = "size", defaultValue = "5") Integer size,
+                        @RequestParam(name = "size", defaultValue = "9") Integer size,
                         @RequestParam(name = "search",required = false) String search
     ) {
 
